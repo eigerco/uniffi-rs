153f207abfb0bce0e67eafcb6725c2aeca0a19a2
Luca Campobasso <lcampobasso@gmail.com>
2023-01-24 14:42:46 +0100

    added docs to ruby and kotlin

--- uniffi_bindgen/src/bindings/kotlin/templates/wrapper.kt
+++ uniffi_bindgen/src/bindings/kotlin/templates/wrapper.kt
@@ -41,6 +41,7 @@ import java.nio.ByteOrder
 {{ type_helper_code }}
 
 {%- for func in ci.function_definitions() %}
+{%- include "TopLevelFuncDocsTemplate.kt" %}
 {%- include "TopLevelFunctionTemplate.kt" %}
 {%- endfor %}
 
